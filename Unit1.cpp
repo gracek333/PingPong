48798721102eda70e56daf3e5458eb1ee51c55b2
Michal Gracon <michalgracon@onet.pl>
2021-05-05 16:53:51 +0200

    Coded move of rackets

--- Unit1.cpp
+++ Unit1.cpp
@@ -14,4 +14,46 @@ __fastcall TPingPong::TPingPong(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
- 
+
+void __fastcall TPingPong::shiftUpLeftRacketTimer(TObject *Sender)
+{
+    if (leftRacket->Top > 10) leftRacket->Top -= 20;        
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::shiftDownLeftRacketTimer(TObject *Sender)
+{
+    if (leftRacket->Top+leftRacket->Height < background->Height-10) leftRacket->Top += 20;        
+}
+//---------------------------------------------------------------------------
+void __fastcall TPingPong::shiftDownRightRacketTimer(TObject *Sender)
+{
+    if (rightRacket->Top+rightRacket->Height < background->Height-10) rightRacket->Top += 20;        
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TPingPong::shiftUpRightRacketTimer(TObject *Sender)
+{
+    if (rightRacket->Top > 10) rightRacket->Top -= 20;        
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TPingPong::FormKeyDown(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+    if (Key == VK_UP) shiftUpRightRacket->Enabled = true;
+    if (Key == 0x41) shiftUpLeftRacket->Enabled = true;
+    if (Key == VK_DOWN) shiftDownRightRacket->Enabled = true;
+    if (Key == 0x5A) shiftDownLeftRacket->Enabled = true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TPingPong::FormKeyUp(TObject *Sender, WORD &Key,
+      TShiftState Shift)
+{
+    if (Key == VK_UP) shiftUpRightRacket->Enabled = false;
+    if (Key == 0x41) shiftUpLeftRacket->Enabled = false;
+    if (Key == VK_DOWN) shiftDownRightRacket->Enabled = false;
+    if (Key == 0x5A) shiftDownLeftRacket->Enabled = false;
+}
+//---------------------------------------------------------------------------
+
